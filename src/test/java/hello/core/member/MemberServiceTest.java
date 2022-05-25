8f007757b266d877831a49093ab3d22c10f1208b
youswim96@gmail.com <youswim96@gmail.com>
2022-05-25 22:52:17 +0900

    12

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
