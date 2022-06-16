56ec824fc7eddf1241ce3e88ad57af2706ce087e
youswim96@gmail.com <youswim96@gmail.com>
2022-06-16 23:54:43 +0900

    128

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,7 @@
 package hello.core.lifecycle;
 
 public class NetworkClient {
+//public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
@@ -10,8 +11,8 @@ public class NetworkClient {
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
+//        connect();
+//        call("초기화 연결 메세지");
     }
 
     public void connect() {
@@ -22,7 +23,29 @@ public class NetworkClient {
         System.out.println("call : " + url + " message = " + message);
     }
 
-    public void disconnect() {
+    public void disConnect() {
         System.out.println("close : " + url);
     }
+
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disConnect();
+    }
+
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        connect();
+//        call("초기화 연결 메세지");
+//    }
+//
+//    @Override
+//    public void destroy() throws Exception {
+//        disconnect();
+//    }
 }
